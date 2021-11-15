3bb446dc450be98d74c32f2a1b684366d398b0a8
Mike Wang <55117208+mikewang928@users.noreply.github.com>
2021-11-15 12:33:42 -0500

    test 1 - 7 done

--- ramp.py
+++ ramp.py
@@ -1,13 +1,35 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, abort
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"hworld"}
+error_message = "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight"
 
-api.add_resource(HelloWorld,"/")
+input_args = reqparse.RequestParser()
+input_args.add_argument("length_unit", choices = ("mm", "inches"), type=str, help=error_message, required=True)
+input_args.add_argument("width", type=float, help=error_message, required=True)
+input_args.add_argument("length", type=float, help=error_message, required=True)
+input_args.add_argument("weight_unit", choices = ("g", "ounces"), type=str, help=error_message, required=True)
+input_args.add_argument("weight", type=float, help=error_message, required=True)
+
+def abort_if_exceeds_length_limit(args):
+        if not str.equals(args, "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight"): 
+            if str.equal(args["length_unit"], 'mm'):
+                length_temp = args["length"]
+            if str.equal(args["length_unit"], 'inches'):
+                length_temp = args["length"]/25.4
+            if length_temp > 380.0:
+                abort(406, "length can not be larger than 380mm or 14.9606 inches")
+
+class Calculator(Resource):
+    def put(self):
+        args = input_args.parse_args()
+        #abort_if_exceeds_length_limit(args)
+        print(args.json())
+        return args   
+
+api.add_resource(Calculator,"/calculator")
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ rampTest.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 09:21:34 2021
+
+@author: wsycx
+"""
+import requests
+import json
+
+BASE = "http://127.0.0.1:5000/"
+try: 
+    from ramp import app 
+    import unittest
+except Exception as e: 
+    print("some modules are missing {} ".format(e))
+    
+class Flasktest(unittest.TestCase):
+    def test_args_none(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : None})
+        self.assertEqual(response.json()['message']['length_unit'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+
+    def test_one_args(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : "mm"})
+        self.assertEqual(response.json()['message']['width'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+    
+    def test_missing_one_args(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : "mm", "width" : 10.0, "length":10.0, "weight_unit": "g"})
+        self.assertEqual(response.json()['message']['weight'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+    
+    def test_all_args_exist_width_str(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : 'mm', "width" : 'width', "length":10.0, "weight_unit": "g", "weight": 10.0})
+        self.assertEqual(response.json()['message']['width'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+
+    def test_arg_length_unit_not_mm_or_inches(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : 'm', "width" : 10.0, "length":10.0, "weight_unit": "g", "weight": 10.0})
+        #print(response.json())
+        self.assertEqual(response.json()['message']['length_unit'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+    
+    def test_arg_weight_unit_not_g_or_ounces(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : 'mm', "width" : 10.0, "length":10.0, "weight_unit": "kg", "weight": 10.0})
+        #print(response.json())
+        self.assertEqual(response.json()['message']['weight_unit'], "input json data format must include (str)length unit -> mm or inches , (float)width, (float)length, (str)weight unit -> g or ounces, (float)weight")
+
+    '''
+    def test_exceeds_length_limit(self):
+        tester = app.test_client(self)
+        response = requests.put(BASE + "calculator", {"length_unit" : 'mm', "width" : 10.0, "length":400.0, "weight_unit": "g", "weight": 10.0})
+        print(response.status_code)
+        self.assertEqual(response.json()['message']['weight_unit'], "length can not be larger than 380mm or 14.9606 inches")
+    '''
+if __name__ == '__main__':
+    unittest.main()
